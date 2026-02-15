efad10c2ff659e51a053a2f6d939ff964e52c3db
david <adamita.david@gmail.com>
2026-02-15 15:02:48 +0200

    Fix for API call timeout

--- frontend/build/web/flutter_service_worker.js
+++ frontend/build/web/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "110982b7145018142a3c29c5aadcb201
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "a534f96ec2366ca37cfcb6bb1a7cee37",
+"flutter_bootstrap.js": "58699ecad7cf4002e0ca67c29be38759",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "9a3befd5132bbb3bc2bb9611246c9add",
 "/": "9a3befd5132bbb3bc2bb9611246c9add",
-"main.dart.js": "29b800638d9ab27be1982c6b8694fa4d",
+"main.dart.js": "69b40331a9d48fb94ac0ee49ed2167f0",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7"};
 // The application shell files that are downloaded before a service worker can
